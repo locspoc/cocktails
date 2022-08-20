0b0f5e7a601ee5c498106776bb6b3e1cde0d2395
Loc Tran <mrlocspoc@gmail.com>
2022-08-20 15:10:12 +0930

    Initial commit

---
+++ build/precache-manifest.560e5d38d067b99d006cfdeeb8de1e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac90a69075ae52bc376672bd6a9f851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01be5c590222281541c8",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "b96953a720430f3617f0",
+    "url": "/static/js/2.d2506600.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/static/js/2.d2506600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01be5c590222281541c8",
+    "url": "/static/js/main.9eddbfc1.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
